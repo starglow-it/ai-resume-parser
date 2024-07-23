0cacb17412d82d815609b8f4f1cf3d2c2d28b58a
starglow-it <starglowventures@gmail.com>
2024-07-22 19:44:23 -0700

    make resume parser component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ResumeParser from './pages/ResumeParser'
+
+function App() {
+  return (
+    <div className="App">
+      <ResumeParser />
+    </div>
+  );
+}
+
+export default App;
